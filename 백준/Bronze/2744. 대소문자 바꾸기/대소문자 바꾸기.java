6272370b9e6c9ccc20161de03cefad10b2b5bc94
chae <noday29@gmail.com>
2024-12-11 21:19:11 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 196 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String result = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isUpperCase(c)) {
+                result += Character.toLowerCase(c);
+            } else {
+                result += Character.toUpperCase(c);
+            }
+        }
+        System.out.println(result);
+    }
+}
