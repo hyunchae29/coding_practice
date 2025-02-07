32254567fac4e58a8f0ff638decc535eb682f9cb
chae <noday29@gmail.com>
2025-02-07 17:37:15 +0900

    [Bronze I] Title: 평균, Time: 304 ms, Memory: 25092 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        double[] score = new double[N];
+        double maxScore = 0;
+        double sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            score[i] = sc.nextDouble();
+            if (score[i] > maxScore) {
+                maxScore = score[i];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            sum += (score[i] / maxScore) * 100;
+        }
+
+        System.out.println(sum / N);   
+        sc.close();
+    }
+}
